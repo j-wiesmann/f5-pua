785baf808f3f77f36cc4baaa868f9bcc59d497b1
billchurch <wmchurch@gmail.com>
2018-02-16 16:17:23 -0500

    for WebSSH2 on TMOS 13.1.0.1 or higher

--- bin/startup_script_webssh_commands.sh
+++ bin/startup_script_webssh_commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Filename: /config/startup_script_tmm_commands.sh
+# Filename: /config/startup_script_webssh_commands.sh
 # Initializes WebSSH2 tmm-to-node listener
 # WebSSHVSIP should be the IP on an existing BIGIP virtual server assigned to
 # the WebSSH2 service.
@@ -10,12 +10,22 @@ export myFileName=$0
 export REMOTEUSER=root
 export WEBSSHVSIP=192.168.20.42
 
+# check to see if we're in /config/startup, if not add ourselves
+IN_STARTUP=`grep startup_script_webssh_commands.sh /config/startup | wc -l`
+if [ $IN_STARTUP -eq 0 ]; then
+  echo Adding script to /config/startup and ensuring correct permissions...
+  logger -p local0.notice -t $myFileName Adding $0 to /config/startup and ensuring correct permissions...
+  chmod 755 /config/startup
+  chmod 755 /config/startup_script_webssh_commands.sh
+  echo /config/startup_script_webssh_commands.sh \& >> /config/startup
+  echo >> /config/startup
+fi
+
 # Limit to 13 times in while-loop, ie. 12 x 10 secs sleep = 2 mins.
 MAX_LOOP=13
 
 while true
 do
-echo hello
 # check to see if tmm interface is up
 IPLINKLIST=$(ip link list tmm 2>&1)
 if [ $? -eq 0 ]; then
@@ -31,6 +41,9 @@ if [ $? -eq 0 ]; then
       logger -p local0.notice -t $myFileName IPADDRADD: FAILURE: $IPADDRADD
     fi
   else
+    echo FAILURE: WEBSSHVSIP not specified.
+    echo Open $0 and set the WEBSSHVSIP and try again
+    echo
     logger -p local0.notice -t $myFileName IPADDRADD: FAILURE: NO WEBSSHVSIP SPECIFIED
   fi
   exit
@@ -47,4 +60,4 @@ fi
 ((MAX_LOOP--))
 done
 
-# End of file /config/startup_script_tmm_commands.sh
+# End of file /config/startup_script_webssh_commands.sh
