87fc84e8bb5aaf51f2092437b8a8494b0a7ad1a2
billchurch <wmchurch@gmail.com>
2018-02-20 15:31:59 -0500

    scripts/build_offline.sh
    This file:
    - copies bin/build_pua.sh to bin/build_pua_offline.sh
    - adds archive header/footer to bin/build_pua_offline.sh
    - creates tar.gz of files specified in ARCHIVE.LIST
    - creates base64 version of tar.gz
    - appends to bin/build_pua_offline.sh

---
+++ scripts/build_offline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# build offline version of build_pua.sh
+
+cd bin/
+
+cp build_pua.sh build_pua_offline.sh
+echo __PUA_ARCHIVE__ >> build_pua_offline.sh
+tar -cvzf - -T ../scripts/ARCHIVE.LIST | base64 >> build_pua_offline.sh
