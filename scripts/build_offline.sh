999948d11e39e5ba45b9dc795253144850e47160
billchurch <wmchurch@gmail.com>
2018-02-21 14:29:19 -0500

    1.0.8

--- scripts/build_offline.sh
+++ scripts/build_offline.sh
@@ -3,8 +3,8 @@
 # build offline version of build_pua.sh
 
 cd bin/
-
 cp build_pua.sh build_pua_offline.sh
+chmod 755 build_pua.sh build_pua_offline.sh
 echo __PUA_ARCHIVE__ >> build_pua_offline.sh
 tar -cvzf - -T ../scripts/ARCHIVE.LIST | base64 >> build_pua_offline.sh
 zip build_pua_offline.zip build_pua_offline.sh
